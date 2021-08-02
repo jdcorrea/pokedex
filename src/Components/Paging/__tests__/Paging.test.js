f8db9c14b0661618f08d6a9aa19d2fcc6fa44aa0
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2021-08-02 01:45:12 -0500

    add: paging history test done

--- src/Components/Paging/__tests__/Paging.test.js
+++ src/Components/Paging/__tests__/Paging.test.js
@@ -4,30 +4,25 @@ import ReactRouterDom from "react-router-dom";
 import Paging from "..";
 import { onClickNextObject, onClickPreviousObject, mockHistory } from "../__mocks__/Paging.mock";
 
-// jest.mock('react-router-dom', () => ({
-//   ...jest.requireActual('react-router-dom'),
-//   useHistory: () => mockHistory
-// }))
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => mockHistory
+}))
 
 describe('<Paging /> Component', () => {
-  xit('Should render the Paging component', () => {
+  it('Should render the Paging component', () => {
     const paging = mount(<Paging/>)
     expect(paging.length).toEqual(1);
   });
 
-  it('should render with default props and click next', () => {
-    const spyNext = jest.spyOn(onClickNextObject, 'onClickNext');
+  it('should click on pokeball ', () => {
+    const spyGoToPokedex  = jest.spyOn(mockHistory, 'push');
     const wrapper = mount(
       <Paging onClickNext={onClickNextObject.onClickNext} onClickPrevious={onClickPreviousObject.onClickPrevious} />
     );
-    const nextButton = wrapper.find('[rol="next"]');
-    expect(wrapper.length).toEqual(1);
-    expect(nextButton.exists()).toEqual(true);
-    nextButton.simulate('click');
-    expect(spyNext).toHaveBeenCalled();
+    const goHomeButton = wrapper.find('[alt="go back to pokedex"]');
+    expect(goHomeButton.exists()).toEqual(true);
+    goHomeButton.simulate('click');
+    expect(spyGoToPokedex).toHaveBeenCalled();
   });
-
-  // it('should render with default props and click on pokeball ', () => {
-    
-  // });
 });
